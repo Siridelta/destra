19480d66a355338ec450709dece1234b280f8cc8
StaticReasons <827391398@qq.com>
2025-05-23 21:48:13 +0800

    clear

--- src/ExprBuild.jl
+++ src/ExprBuild.jl
@@ -1,15 +1,5 @@
 module ExprBuild
 
-# 导出核心组件
-export ExprNode, AssignmentNode, EquationNode
-export @expl, parse_expression
-export find_dependencies, substitute
-export compile_to_desmos, save_to_desmos_state
 
-# 包含核心组件
-include("core/ExprNode.jl")
-include("core/Macros.jl")
-include("core/Dependencies.jl")
-include("core/DesmosCompiler.jl")
 
 end # module
