fd9aa27033d07763e3da866980716c47f4589def
Static1248 <static1248@qq.com>
2026-01-08 16:24:01 +0800

    wip 8

--- destra.js/variant-drafts/v-id-mutable/src/core/expr-dsl-new/parse-formula/lexing/lexer.ts
+++ destra.js/variant-drafts/v-id-mutable/src/core/expr-dsl-new/parse-formula/lexing/lexer.ts
@@ -2,7 +2,6 @@ import { Lexer } from "chevrotain";
 import { Whitespace, SingleLineComment, NumberLiteral, Placeholder, CustomIdentifier } from "../tokens/others";
 import { keywords } from "../tokens/keywords";
 import { opAndPuncs } from "../tokens/op-and-puncs";
-import { IMult } from "../tokens/imult";
 import { reservedWords } from "../tokens/reserved-words";
 
 export const tokensList = [
@@ -21,8 +20,6 @@ export const tokensList = [
     ...reservedWords,
     
     CustomIdentifier,
-
-    IMult
 ]
 
 export const lexer = new Lexer(tokensList);
--- destra.js/variant-drafts/v-id-mutable/src/core/expr-dsl-new/parse-formula/tokens/imult.ts
+++
@@ -1,7 +0,0 @@
-import { createToken, Lexer } from "chevrotain";
-
-// Implicit Multiply Token, to be inserted in post-lexing phase
-export const IMult = createToken({
-    name: "IMult",
-    pattern: Lexer.NA
-});
